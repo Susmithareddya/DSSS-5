45fa884598c01fb2be5d86d8fbe700633f4fc731
Susmithareddya <93251900+Susmithareddya@users.noreply.github.com>
2022-12-01 23:34:12 +0100

    commiting requirments file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='DSSS-5',
+    version='1.0.0',
+    packages=['snowflake'],
+    url='',
+    license='',
+    author='arumalla',
+    author_email='susmithar456@gmail.com',
+    description='setup of snowflake'
+)

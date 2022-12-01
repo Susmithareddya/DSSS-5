af3070ec7f78d18c4e44ff9acf41b99a7649f3c8
Susmithareddya <93251900+Susmithareddya@users.noreply.github.com>
2022-12-01 23:36:00 +0100

    commiting setup files

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from distutils.core import setup
+from setuptools import find_packages
 
 setup(
     name='DSSS-5',
     version='1.0.0',
     packages=['snowflake'],
+    package = find_packages(),
+    install_requires = ["numpy","turtles"],
     url='',
     license='',
     author='arumalla',
